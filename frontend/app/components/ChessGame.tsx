8766e20ac1b7d14800aacd04761df5be441180cb
Rajan66 <rajanmaharjan042@gmail.com>
2024-10-20 18:02:54 +0545

    feat: added error handling for invalid moves

--- frontend/app/components/ChessGame.tsx
+++ frontend/app/components/ChessGame.tsx
@@ -1,26 +1,37 @@
 import { Chessboard } from "react-chessboard";
 import { Chess } from "chess.js";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function ChessGame() {
     const [game, setGame] = useState(new Chess());
     const [boardPosition, setBoardPosition] = useState(game.fen());
 
     const onDrop = (sourceSquare: string, targetSquare: string) => {
-        console.log(`sourceSquare: ${sourceSquare}, targetSquare: ${targetSquare}`);
+        try {
 
-        const move = game.move({
-            from: sourceSquare,
-            to: targetSquare,
-            promotion: 'q' // for simplicity
-        });
+            console.log(`sourceSquare: ${sourceSquare}, targetSquare: ${targetSquare}`);
 
-        if (move === null) {
-            return false; // Invalid move
+            const move = game.move({
+                from: sourceSquare,
+                to: targetSquare,
+                promotion: 'q' // for simplicity
+            });
+
+            if (move === null) {
+                return false; // Invalid move
+            }
+
+            setBoardPosition(game.fen());
+            // display the position of the piece
+            // the move function moves the piece but the position is required to display the state on the board
+
+            return true; // Valid move
+        } catch (error: any) {
+            toast.error("Invalid move")
+            console.log(error)
+            return false
         }
-
-        setBoardPosition(game.fen());
-        return true; // Valid move
     };
 
     return (
